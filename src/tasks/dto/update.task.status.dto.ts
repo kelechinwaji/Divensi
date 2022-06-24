6180f20683beebd8763d16cbf406a763f1afd3ff
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-06-24 23:03:32 +0100

    implemented DTO on patch controller

--- src/tasks/dto/update.task.status.dto.ts
+++ src/tasks/dto/update.task.status.dto.ts
@@ -1,7 +1,7 @@
 import { IsEnum } from "class-validator";
 import { TaskStatus } from "../task.model";
 
-export class updateTaskStatusDto{
+export class UpdateTaskStatusDto{
     @IsEnum(TaskStatus)
     status: TaskStatus
 }
