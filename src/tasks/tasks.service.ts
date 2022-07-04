f47070085d90d2f002dd5b0f2f4a96beed9c8453
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-07-04 17:39:08 +0100

    implemeted update task service logic

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -36,49 +36,16 @@ export class TasksService {
  
   async deleteTask(id: string): Promise<void>{
     const result = await this.tasksRepository.delete(id);
-    console.log(result);
+    if(result.affected === 0){
+        throw new NotFoundException(`Task with user id ${id} not found`)
+    }
     
   }
 
-  // getTaskWithFilters(filterDto: GetTaskFilterDto): Task[]{
-  //     const { status, search} = filterDto;
-  //     let tasks = this.getAllTask();
-  //     if(status){
-  //         tasks = tasks.filter((task) => task.status === status)
-  //     }
-  //     if(search){
-  //        tasks = tasks.filter((task)=>{
-  //         if (task.title.includes(search) || task.description.includes(search)){
-  //             return true;
-  //         }
-
-  //         return false
-  //        })
-  //     }
-  //     return tasks;
-  // }
-
-  // createTask(createTaskDto: CreateTaskDto):Task{
-  //     const {title, description} = createTaskDto
-  //     const task:Task = {
-  //         id: uuid(),
-  //         title,
-  //         description,
-  //         status: TaskStatus.OPEN
-  //     };
-
-  //     this.tasks.push(task);
-  //     return task
-  // }
-
-  //  updateTaskStatus(id: string, status: TaskStatus){
-  //     const task = this.getTaskById(id);
-  //     task.status = status;
-  //     return task
-  //  }
-
-  // deleteTask(id: string): void{
-  //     const found = this.getTaskById(id)
-  //     this.tasks = this.tasks.filter((task) => task.id !== found.id)
-  // }
+  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task>{
+    const task = await this.getTaskById(id);
+    task.status = status;
+    await this.tasksRepository.save(task);
+    return task;
+  }
 }
