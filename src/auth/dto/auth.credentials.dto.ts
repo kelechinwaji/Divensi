c977e987d923ce83e56bcdc6d61a48172e9ef7f1
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-07-06 16:14:05 +0100

    implemented user signup validation requirement

--- src/auth/dto/auth.credentials.dto.ts
+++ src/auth/dto/auth.credentials.dto.ts
@@ -1,5 +1,14 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class AuthCredentialsDto{
+    @IsString()
+    @MinLength(4)
+    @MaxLength(10)
     username: string;
+
+    @IsString()
+    @MinLength(6)
+    @MaxLength(10)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
     password: string
 }
