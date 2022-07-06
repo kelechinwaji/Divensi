0bf937f1ee25b1eb64add157263b37c66a25d07b
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-07-06 14:56:20 +0100

    implemented service logic for user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth.credentials.dto';
 import { User } from './user.entity';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        @InjectRepository(User)
-        private tasksRepository: Repository<User>,
-      ) {}
-    
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.userRepository.create({
+      username,
+      password,
+    });
+
+    await this.userRepository.save(user);
+    return user;
+  }
 }
